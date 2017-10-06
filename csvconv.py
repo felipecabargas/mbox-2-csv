f11d6b79edd39c4556fb637452526f840d56a216
A. Felipe Cabargas <felipe.cabargas@gmail.com>
2017-10-06 18:40:44 -0300

    Add: first commit

---
+++ csvconv.py
@@ -0,0 +1,8 @@
+import mailbox
+import csv
+
+writer = csv.writer(open("mbox-output.csv", "wb"))
+
+for message in mailbox.mbox('Follow up.mbox'):
+	if message['from'] != "Bridget Letts <bridget@groupraise.org>":
+		writer.writerow([message['Date'],message['X-Gmail-Labels'], message['subject'], message['from'], message['body']])
